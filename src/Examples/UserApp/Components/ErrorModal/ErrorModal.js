4ae4a4d1da36f9c1415467eec69f37954f461cc3
Challa,Venu <venuchalla1993@gmail.com>
2022-10-02 04:40:56 -0500

    updated with user example with react portals

--- src/Examples/UserApp/Components/ErrorModal/ErrorModal.js
+++ src/Examples/UserApp/Components/ErrorModal/ErrorModal.js
@@ -1,11 +1,14 @@
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import Classes from './ErrorModal.module.css'
-const ErrorModal = (props) => {
-    return (
-    <>
-    <div className= {Classes.backdrop} onClick ={props.closeErrorModal}></div>
-    <Card className ={Classes.modal}>
+import React from "react";
+import ReactDOM from 'react-dom'
+const BackDrop = (props) => {
+    return (<div className={Classes.backdrop} onClick={props.closeErrorModal}></div>)
+}
+
+const Modal = (props) => {
+    return (<Card className={Classes.modal}>
         <header className={Classes.header}>
             <h2>{props.title}</h2>
         </header>
@@ -13,10 +16,17 @@ const ErrorModal = (props) => {
             {props.message}
         </div>
         <footer className={Classes.actions}>
-            <Button onClick = {props.closeErrorModal}>Okay</Button>
+            <Button onClick={props.closeErrorModal}>Okay</Button>
         </footer>
-    </Card>
-    </>)
+    </Card>)
+}
+const ErrorModal = (props) => {
+    return (<React.Fragment>
+        {ReactDOM.createPortal(<BackDrop closeErrorModal={props.closeErrorModal}></BackDrop>,
+            document.getElementById('backDrop-root'))}
+        {ReactDOM.createPortal(<Modal title={props.title} message={props.message} closeErrorModal={props.closeErrorModal}></Modal>,
+            document.getElementById('modal-root'))}
+    </React.Fragment>)
 }
 
 export default ErrorModal;
