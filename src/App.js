3079d9e57bde0bb6888f59752017351e441b883e
venuchalla <venuchalla1993@gmail.com>
2022-11-23 22:12:10 -0600

    added code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
-import React from 'react';
-import Footer from './Components/Layout/Footer-Component/Footer';
-import MealsMainApp from './Components/MealsMainApp';
-import UserFinderMain from './Examples/ClassComponentExample/UserFinderMain';
-import CustomhooksMain from './Examples/CustomHooksExample/CustomHooksMain';
-import CustomHooksHttpExampleMain from './Examples/CustomHooksHttpExample/CustomHooksHttpExampleMain';
-import ExpenseTracker from './Examples/ExpenseTracker/ExpenseTracker';
-import HttpRequestExample from './Examples/HttpRequestsExample/HttpRequestExample';
-import StyleExample from './Examples/StylesExample/StyleExample';
-import UserMain from './Examples/UserApp/UserMain/UserMain';
-import "./App.css"
-import FormExampleMain from './Examples/FormExample/FormExampleMain';
-import AppHeader from './Components/Layout/App-Header/AppHeader';
-
+import React from "react";
+import Footer from "./Components/Layout/Footer-Component/Footer";
+import MealsMainApp from "./Components/MealsMainApp";
+import UserFinderMain from "./Examples/ClassComponentExample/UserFinderMain";
+import CustomhooksMain from "./Examples/CustomHooksExample/CustomHooksMain";
+import CustomHooksHttpExampleMain from "./Examples/CustomHooksHttpExample/CustomHooksHttpExampleMain";
+import ExpenseTracker from "./Examples/ExpenseTracker/ExpenseTracker";
+import HttpRequestExample from "./Examples/HttpRequestsExample/HttpRequestExample";
+import StyleExample from "./Examples/StylesExample/StyleExample";
+import UserMain from "./Examples/UserApp/UserMain/UserMain";
+import "./App.css";
+import FormExampleMain from "./Examples/FormExample/FormExampleMain";
+import AppHeader from "./Components/Layout/App-Header/AppHeader";
 
 function App() {
 
@@ -31,13 +29,26 @@ function App() {
    }
  */
 
-  return (<>
-    <AppHeader></AppHeader>
-    <section style={{ marginTop: "5rem" }}>
-      <MealsMainApp></MealsMainApp>
-    </section>
-    <Footer><div><p> we are in foot block</p></div></Footer>
-  </>
+  return (
+    <>
+      <AppHeader></AppHeader>
+      <section style={{ marginTop: "5rem" }}>
+        <MealsMainApp></MealsMainApp>
+        <ExpenseTracker></ExpenseTracker>
+        <StyleExample></StyleExample>
+        <UserMain></UserMain>
+        <UserFinderMain></UserFinderMain>
+        <HttpRequestExample></HttpRequestExample>
+        <CustomhooksMain></CustomhooksMain>
+        <CustomHooksHttpExampleMain></CustomHooksHttpExampleMain>
+        <FormExampleMain></FormExampleMain>
+      </section>
+      <Footer>
+        <div>
+          <p> we are in foot block</p>
+        </div>
+      </Footer>
+    </>
   );
 }
 
--- src/Examples/FormExample/SimpleInput.js
+++ src/Examples/FormExample/SimpleInput.js
@@ -1,76 +1,81 @@
-import {  useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-    const [enteredName, setEnterdName] = useState('');
-    const [inputIsValid, setInputIsValid] = useState(false);
-    const [inputTouched, setInputTouched] = useState(false);
-    const [enteredEmail, setEnterdEmail] = useState('');
-    const [inputEmailTouched, setInputEmailIsTocuhed] = useState(false);
-    const validateEmail = (email) => {
-        return email.trim() === ''
-    }
-    let inputEmailIsNotValid = validateEmail(enteredEmail);
-    const inputRef = useRef();
-    const emailRef = useRef();
+  const [enteredName, setEnterdName] = useState("");
+  const [inputIsValid, setInputIsValid] = useState(false);
+  const [inputTouched, setInputTouched] = useState(false);
+  const [enteredEmail, setEnterdEmail] = useState("");
+  const [inputEmailTouched, setInputEmailIsTocuhed] = useState(false);
 
-    const nameChangeHandler = (event) => {
-        setEnterdName(event.target.value)
-        validate(event.target.value)
+  const validateEmail = (email) => {
+    return email.trim() === "";
+  };
+  let inputEmailIsNotValid = validateEmail(enteredEmail);
+  const inputRef = useRef();
+  const emailRef = useRef();
 
-    }
-    const onBlurHandler = (event) => {
-        validate(enteredName)
-    }
-    const onBlurEmailHandler = (event) => {
-        inputEmailIsNotValid = validateEmail(enteredEmail)
-    }
-    const validate = (value) => {
-        setInputTouched(true);
-        let valid = value.trim() !== ''
-        setInputIsValid(valid)
-    }
-    const submitHandler = (event) => {
-        setInputEmailIsTocuhed(true)
-        console.log("clicked on submit")
-        event.preventDefault();
-        validate(enteredName);
-        inputEmailIsNotValid = validateEmail(event.target.value);
-    }
-    const emailChangeHandler = (event) => {
-        setInputEmailIsTocuhed(true);
-        setEnterdEmail(event.target.value)
-        inputEmailIsNotValid = validateEmail(event.target.value);
-    }
-    
-   
-    let showError = !inputIsValid && inputTouched
-    let showEmailError = inputEmailTouched && inputEmailIsNotValid;
-    const classes = showError ? "form-control invalid" : "form-control"
-    return (
-        <form autoComplete="off">
-            <div className={classes}>
-                <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' ref={inputRef}
-                    onChange={nameChangeHandler}
-                    value={enteredName}
-                    onBlur={onBlurHandler}
-                />
-                {showError ? (<p className="error-text">Name is Empty</p>) : null}
-            </div>
-            <div className={classes}>
-                <label htmlFor='email'>Your Email</label>
-                <input type='text' id='email' ref={emailRef}
-                    onChange={emailChangeHandler}
-                    onBlur ={onBlurEmailHandler}
-                    value={enteredEmail}
-                />
-                {showEmailError ? (<p className="error-text">Email is Empty</p>) : null}
-            </div>
-            <div className="form-actions">
-                <button onClick={submitHandler}>Submit</button>
-            </div>
-        </form>
-    );
+  const nameChangeHandler = (event) => {
+    setEnterdName(event.target.value);
+    validate(event.target.value);
+  };
+  const onBlurHandler = (event) => {
+    validate(enteredName);
+  };
+  const onBlurEmailHandler = (event) => {
+    inputEmailIsNotValid = validateEmail(enteredEmail);
+  };
+  const validate = (value) => {
+    setInputTouched(true);
+    let valid = value.trim() !== "";
+    setInputIsValid(valid);
+  };
+  const submitHandler = (event) => {
+    setInputEmailIsTocuhed(true);
+    console.log("clicked on submit");
+    event.preventDefault();
+    validate(enteredName);
+    inputEmailIsNotValid = validateEmail(event.target.value);
+  };
+  const emailChangeHandler = (event) => {
+    setInputEmailIsTocuhed(true);
+    setEnterdEmail(event.target.value);
+    inputEmailIsNotValid = validateEmail(event.target.value);
+  };
+
+  let showError = !inputIsValid && inputTouched;
+  let showEmailError = inputEmailTouched && inputEmailIsNotValid;
+  const classes = showError ? "form-control invalid" : "form-control";
+  return (
+    <form autoComplete="off">
+      <div className={classes}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={inputRef}
+          onChange={nameChangeHandler}
+          value={enteredName}
+          onBlur={onBlurHandler}
+        />
+        {showError ? <p className="error-text">Name is Empty</p> : null}
+      </div>
+      <div className={classes}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="text"
+          id="email"
+          ref={emailRef}
+          onChange={emailChangeHandler}
+          onBlur={onBlurEmailHandler}
+          value={enteredEmail}
+        />
+        {showEmailError ? <p className="error-text">Email is Empty</p> : null}
+      </div>
+      <div className="form-actions">
+        <button onClick={submitHandler}>Submit</button>
+      </div>
+    </form>
+  );
 };
 
 export default SimpleInput;
