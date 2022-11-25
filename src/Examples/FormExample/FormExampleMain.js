6479fe55d7f0144ff82c51e2f90e9991cda71f7f
venuchalla <venuchalla1993@gmail.com>
2022-11-25 16:42:57 -0600

    updated code

--- src/Examples/FormExample/FormExampleMain.js
+++ src/Examples/FormExample/FormExampleMain.js
@@ -10,6 +10,8 @@ const FormExampleMain = (props) => {
       <div className="app">
         <SimpleInput></SimpleInput>
         <br></br>
+        <h2 style={{textAlign:"center"}}> Form example using Hooks</h2>
+        <br></br>
         <BasicForm></BasicForm>
       </div>
     </>
