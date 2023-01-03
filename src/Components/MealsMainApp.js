ac8fa3e9441fca09f2a7e6b030a96dec7157f83f
“Venu” <Venuchalla1993@gmail.com>
2023-01-03 17:42:01 -0600

    updated name with dynamic routing

--- src/Components/MealsMainApp.js
+++ src/Components/MealsMainApp.js
@@ -1,29 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cart from "./Cart/Cart";
 import CartProvider from "./Store/CartProvider";
 import Header from "./Layout/Header-Component/Header";
 import Meals from "./Meals/Meals";
-const MealsMainApp =(props)=>{
-    const [cartShow, setCartShow] = useState(false)
-    const closeCartModal = () => {
-        setCartShow(false)
-      }
-    
-    
-      const showCartModal = () => {
-        setCartShow(true)
-      }
-    
-      const showCartElement = cartShow ? (<Cart onCloseButton={closeCartModal}></Cart>) : ""
+const MealsMainApp = (props) => {
+  const [cartShow, setCartShow] = useState(false);
+  const closeCartModal = () => {
+    setCartShow(false);
+  };
+  useEffect(() => {
+    document.title = "Food Order Application";
+  }, []);
 
-      return (
-        <CartProvider>
-          {showCartElement}
-          <Header onCartButtonClickHandler={showCartModal}></Header>
-          <main>
-          <Meals></Meals>
-          </main>
-        </CartProvider>
-      );
-}
+  const showCartModal = () => {
+    setCartShow(true);
+  };
+
+  const showCartElement = cartShow ? (
+    <Cart onCloseButton={closeCartModal}></Cart>
+  ) : (
+    ""
+  );
+
+  return (
+    <CartProvider>
+      {showCartElement}
+      <Header onCartButtonClickHandler={showCartModal}></Header>
+      <main>
+        <Meals></Meals>
+      </main>
+    </CartProvider>
+  );
+};
 export default MealsMainApp;
